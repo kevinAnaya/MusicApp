ead03861e87b8f4763635340268b338eedbcc7cf
Kevin Anaya <kevinanaya278@gmail.com>
2021-07-02 14:17:37 -0500

    peticion get en el servicio para obtener artistas

--- src/app/music/services/spotify.service.ts
+++ src/app/music/services/spotify.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +9,25 @@ export class SpotifyService {
 
   public url: string = 'https://api.spotify.com/v1/search?query=metallica&limit=20&type=artist';
 
+  artistas: any[] = [];
+
   constructor( private http: HttpClient) { 
     console.log('Servicio spoty listo');
     
   }
 
   getArtistas(){    
-    return this.http.get(this.url)
+    let headers = new HttpHeaders({
+      'authorization' : 'Bearer BQDAe1cDguIq1fVKQpTzZ-DRskcv1v9Bz4wxKJDdVTxr3M2LfD0zSJSVMluCnY_kUXuLiIpDkHLPLLoRtJU'
+    })
+
+    return this.http.get(this.url, { headers })
+               .pipe(
+                 map( (resp:any ) => {
+                   this.artistas = resp.artists.items 
+                   return this.artistas
+                 })
+               )
   }
 
 }
